48ac2fbe615ae27093c1923ca79601c3d568a3fc
leisyarcia <leisyarcia@gmail.com>
2021-03-30 01:50:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,52 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var current = null;
+document.querySelector("#email").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "240 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
+document.querySelector("#password").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "240 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
+document.querySelector("#submit").addEventListener("focus", function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: "path",
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: "easeOutQuart"
+    },
+    strokeDasharray: {
+      value: "530 1386",
+      duration: 700,
+      easing: "easeOutQuart"
+    }
+  });
+});
